5b9e4d3d8ac96a5762db6cb06dce975e754307f1
mona <ekmonapark@gmail.com>
2023-05-24 16:08:44 +0900

    Initialize java + gradle project
    fastcampus example

---
+++ src/main/java/com/mona/fastcampus/FastcampusJavaGradleProjectExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mona.fastcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusJavaGradleProjectExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcampusJavaGradleProjectExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mona/fastcampus/FastcampusJavaGradleProjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mona.fastcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusJavaGradleProjectExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
